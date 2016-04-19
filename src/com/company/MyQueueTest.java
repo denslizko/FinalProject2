5d574673aac5a7431a7b5341aa87a8f956e3be82
Andrew Danylenko <wdanylenko@gmail.com>
2016-04-19 21:05:12 +0300

    add

---
+++ src/com/company/MyQueueTest.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by User on 19.04.2016.
+ */
+public class MyQueueTest extends MyQueue {
+
+}
---
+++ src/com/company/QueueTest.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by User on 19.04.2016.
+ */
+public class QueueTest extends Queue {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFront() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRear() throws Exception {
+
+    }
+
+    @Test
+    public void testIsFull() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSize() throws Exception {
+
+    }
+}
