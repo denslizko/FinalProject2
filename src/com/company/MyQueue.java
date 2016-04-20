4ebee062f147fc5ad22d91c9ef85aeb88e99eb40
DenSlizko <denis.slizko@gmail.com>
2016-04-20 22:24:08 +0300

    Visualization added 3 try + catch added

--- src/com/company/MyQueue.java
+++ src/com/company/MyQueue.java
@@ -12,17 +12,16 @@ public class MyQueue {
             boolean forStart = false;
             boolean rateApp = false;
 
-            System.out.println("Hi! You now in Group #2 Final Project, and u see FIFO realization");
+            System.out.println("Hi! You now in Group #2 Final Project, and u see FIFO(Queue) realization");
             System.out.println("NOTICE: In the end pls rate this small but created with many love project :)");
             System.out.println("----------------------------------------------------------------------------");
             System.out.println("Here u see detailed instruction, later it will be short variant. Pls read all description...");
             System.out.println("----------------------------------------------------------------------------");
             System.out.println("Insert number for choose u need: ");
             System.out.println("Enter '1' for push new item (we use ony numbers)");
-            System.out.println("Enter '2' for push many items in Queue");
-            System.out.println("Enter '3' for pop next item (in FIFO first item)");
-            System.out.println("Enter '4' if u want 2 see front item in Queue");
-            System.out.println("Enter '5' to stop this madness");
+            System.out.println("Enter '2' for pop next item (in FIFO first item)");
+            System.out.println("Enter '3' if u want 2 see front item in Queue");
+            System.out.println("Enter '4' to stop this madness");
 
 
             while (!forStart) {
@@ -30,10 +29,9 @@ public class MyQueue {
         try {
                 System.out.println("Choose: ");
                 System.out.print("| '1'  push |");
-                System.out.print(" '2'  push many |");
-                System.out.print(" '3'  pop |");
-                System.out.print(" '4' front item |");
-                System.out.println(" '5' stop |");
+                System.out.print(" '2'  pop |");
+                System.out.print(" '3' front item |");
+                System.out.println(" '4' stop |");
 
                 Scanner scanner = new Scanner(System.in);
                 int choose = scanner.nextInt();
@@ -43,46 +41,47 @@ public class MyQueue {
                     System.out.println("Size of Queue " + myQueue.getSize());
                 }
 
-                if (choose == 2){ // Добавляем много элементов в конец, нужно проверить логику
-                    System.out.println("------------------------");
-                    System.out.println("SORRY: We haven`t realization for this function now, still working on it");
-                    System.out.println("------------------------");
-                }
-
-                if (choose == 3){ // Удаляем первый элемент
+                if (choose == 2){ // Удаляем первый элемент
                     int e = myQueue.remove();
                     System.out.println("Remove: " + e);
                 }
-                if (choose == 4) { // Показ первого элемента
+                if (choose == 3) { // Показ первого элемента
                     System.out.println("Front item is: " + myQueue.getFront());
                 }
 
-                if (choose == 5) { // Выходим из цикла
+                if (choose == 4) { // Выходим из цикла
                     forStart = true;
                 }
             }catch (InputMismatchException inputException){
-            System.out.println("Error! Please enter an integer number");
-        }}
+                    System.out.println("Error! Please enter an integer number");
+                }
+            }
 
-            System.out.println("Please rate our work, where 0 - the Worstest number and 10 - Bestest number");
-            Scanner rated = new Scanner(System.in);
-            int rate = rated.nextInt();
-            System.out.println("Your rate is: " + rate);
+
 
             while(!rateApp){
 
-                if (rate == 10){
-                    System.out.println("Thanks! It`s very big pleasure to have time with you! :)");
-                    rateApp = true;
-                }
+                try {
+                    System.out.println("Please rate our work, where 0 - the Worstest number and 10 - Bestest number");
+                    Scanner rated = new Scanner(System.in);
+                    int rate = rated.nextInt();
+                    System.out.println("Your rate is: " + rate);
 
-                if (rate <10){
-                    System.out.println("Pls leave a comment for u rate");
-                    Scanner comment = new Scanner(System.in);
-                    String comments = comment.nextLine();
-                    System.out.println("Pls save the screenshot with your comment and send us in skype. GL! ");
-                    System.out.println("Comment: " + comments);
-                    rateApp = true;
+                    if (rate == 10) {
+                        System.out.println("Thanks! It`s very big pleasure to have time with you! :)");
+                        rateApp = true;
+                    }
+
+                    if (rate < 10) {
+                        System.out.println("Pls leave a comment for u rate");
+                        Scanner comment = new Scanner(System.in);
+                        String comments = comment.nextLine();
+                        System.out.println("Pls save the screenshot with your comment and send us in skype. GL! ");
+                        System.out.println("Comment: " + comments);
+                        rateApp = true;
+                    }
+                }catch (InputMismatchException inputException){
+                    System.out.println("Error! Please enter an integer number");
                 }
             }
         }
--- src/com/company/MyStack.java
+++ src/com/company/MyStack.java
@@ -6,32 +6,83 @@ import java.util.Scanner;
 
 public class MyStack {
     public static void main(String[] args) {
-        try {
-            Scanner scannerStart = new Scanner(System.in);
-            System.out.println("Enter '1' if you want to make a List of numbers or enter another number if you don't want to make a List: ");
-            int EnteringNumbers = scannerStart.nextInt();
 
-            if (EnteringNumbers == 1) {
-        Stack mStack = new Stack(10);
+        boolean forStart = false;
+        boolean rateApp = false;
 
-        mStack.addElement();
-        mStack.addElement();
-        mStack.addElement();
-        mStack.addElement();
+        Stack mystack = new Stack(100);
 
-        mStack.deleteElement();
+        System.out.println("Hi! You now in Group #2 Final Project, and u see LIFO(Stack) realization");
+        System.out.println("NOTICE: In the end pls rate this small but created with many love project :)");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("Here u see detailed instruction, later it will be short variant. Pls read all description...");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("Insert number for choose u need: ");
+        System.out.println("Enter '1' for add new item (we use ony numbers)");
+        System.out.println("Enter '2' for delete last added item (in LIFO first item)");
+        System.out.println("Enter '3' if u want 2 see top item in Stack");
+        System.out.println("Enter '4' to stop this madness");
 
-        System.out.print("Stack: ");
-        while (!mStack.isEmpty()) {
-            int value = mStack.deleteElement();
-            System.out.print(value);
-            System.out.print(" ");
+        while (!forStart) {
+
+            try {
+                System.out.println("Choose: ");
+                System.out.print("| '1'  add |");
+                System.out.print(" '2'  delete |");
+                System.out.print(" '3' top item |");
+                System.out.println(" '4' stop |");
+
+                Scanner scanner = new Scanner(System.in);
+                int choose = scanner.nextInt();
+
+                if (choose == 1) { // Добавляем элемент в начало
+                    mystack.addElement();
+                    System.out.println("Number '" + mystack.readTop() + "' added successful");
+                }
+
+                if (choose == 2) { // Удаляем первый элемент
+                    int e = mystack.deleteElement();
+                    System.out.println("Delete: " + e);
+                    System.out.println("Now top item is: " + mystack.readTop());
+                }
+                if (choose == 3) { // Показ первого элемента
+                    System.out.println("Top item is: " + mystack.readTop());
+                }
+
+                if (choose == 4) { // Выходим из цикла
+                    forStart = true;
+                }
+            } catch (InputMismatchException inputException) {
+                System.out.println("Error! Please enter an integer number");
+            }
+
         }
-        System.out.println("");
-    }else {
-                System.out.println("Thank you for your attention");
 
-            }}catch (InputMismatchException inputException){
-            System.out.println("Error! Please enter an integer number");
+        while (!rateApp) {
+
+            try {
+            System.out.println("Please rate our work, where 0 - the Worstest number and 10 - Bestest number");
+            Scanner rated = new Scanner(System.in);
+            int rate = rated.nextInt();
+            System.out.println("Your rate is: " + rate);
+
+                if (rate == 10) {
+                    System.out.println("Thanks! It`s very big pleasure to have time with you! :)");
+                    rateApp = true;
+                }
+
+                if (rate < 10) {
+                    System.out.println("Pls leave a comment for u rate");
+                    Scanner comment = new Scanner(System.in);
+                    String comments = comment.nextLine();
+                    System.out.println("Pls save the screenshot with your comment and send us in skype. GL! ");
+                    System.out.println("Comment: " + comments);
+                    rateApp = true;
+                }
+
+            }catch (InputMismatchException inputException) {
+                System.out.println("Error! Please enter an integer number");
+            }
         }
-    }}
+    }
+}
